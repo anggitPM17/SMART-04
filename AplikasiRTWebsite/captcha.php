78f9421d2955b8be598ca82594f5057cfe85ee9f
anggitPM17 <167511000+anggitPM17@users.noreply.github.com>
2025-04-20 15:40:02 +0700

    Create captcha.php

---
+++ AplikasiRTWebsite/captcha.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// Menghasilkan dua angka acak untuk soal matematika
+$angka1 = rand(1, 10);
+$angka2 = rand(1, 10);
+
+// Membuat soal matematika (penjumlahan)
+$question = "$angka1 + $angka2";
+$answer = $angka1 + $angka2;
+
+// Menyimpan jawaban yang benar di sesi
+$_SESSION['captcha_answer'] = $answer;
+
+// Menampilkan soal matematika pada halaman
+echo json_encode(['question' => $question]);
+?>
